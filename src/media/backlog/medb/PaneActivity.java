e9ecd7157c81584f27e180b8e89248b68b85cdc5
Arin Tsikretsis <adt608@hotmail.com>
2013-11-28 17:41:48 -0600

    Framework for action bar buttons

---
+++ src/media/backlog/medb/PaneActivity.java
@@ -0,0 +1,13 @@
+package media.backlog.medb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Arin on 11/28/13.
+ */
+public class PaneActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
