0b42f659e7a30f10ef628726ecc2363b91d51a90
Arin Tsikretsis <adt608@hotmail.com>
2013-11-28 15:52:04 -0600

    Home bar implemented with fragments.

--- src/media/backlog/medb/HomeBar.java
+++ src/media/backlog/medb/HomeBar.java
@@ -6,15 +6,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 /**
  * Created by Arin on 11/26/13.
  */
-public class HomeBar extends Fragment {
+public class HomeBar extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_bar, container, false);
+        View v = inflater.inflate(R.layout.home_bar, container, false);
+        ImageButton homeButton = (ImageButton) v.findViewById(R.id.homeButton);
+        ImageButton discoverButton = (ImageButton) v.findViewById(R.id.discoverButton);
+        ImageButton organizeButton = (ImageButton) v.findViewById(R.id.organizeButton);
+        ImageButton searchButton = (ImageButton) v.findViewById(R.id.searchButton);
+        homeButton.setOnClickListener(this);
+        discoverButton.setOnClickListener(this);
+        organizeButton.setOnClickListener(this);
+        searchButton.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.homeButton:
+                homeButton(v);
+                break;
+            case R.id.organizeButton:
+                organizeButton(v);
+                break;
+            case R.id.discoverButton:
+                discoverButton(v);
+                break;
+            case R.id.searchButton:
+                searchButton(v);
+                break;
+        }
+    }
+
+    public void searchButton(View view) {
+        Intent intent = new Intent(this.getActivity(), SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void organizeButton(View view) {
+        Intent intent = new Intent(this.getActivity(), OrganizeActivity.class);
+        startActivity(intent);
+    }
+
+    public void discoverButton(View view) {
+        Intent intent = new Intent(this.getActivity(), DiscoverActivity.class);
+        startActivity(intent);
+    }
+
+    public void homeButton(View view) {
+        Intent intent = new Intent(this.getActivity(), MainActivity.class);
+        startActivity(intent);
     }
 
 }
--- src/media/backlog/medb/MainActivity.java
+++ src/media/backlog/medb/MainActivity.java
@@ -1,10 +1,7 @@
 package media.backlog.medb;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -14,31 +11,4 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void searchButton(View view) {
-        Intent intent = new Intent(this, SearchActivity.class);
-        startActivity(intent);
-    }
-
-    public void organizeButton(View view) {
-        Intent intent = new Intent(this, OrganizeActivity.class);
-        startActivity(intent);
-    }
-
-    public void discoverButton(View view) {
-        Intent intent = new Intent(this, DiscoverActivity.class);
-        startActivity(intent);
-    }
-
-    public void homeButton(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
