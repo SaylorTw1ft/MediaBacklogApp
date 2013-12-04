fef478f911eb3b9db65ea6f801a3b42e3c81de6a
SaylorTw1ft <kbatra@live.com>
2013-12-04 13:19:06 -0600

    Ignoring bin and gen folders

--- gen/media/backlog/medb/R.java
+++ gen/media/backlog/medb/R.java
@@ -10,67 +10,183 @@ package media.backlog.medb;
 public final class R {
     public static final class attr {
     }
-
+    public static final class color {
+        public static final int book_color=0x7f050003;
+        public static final int discover_color=0x7f050004;
+        public static final int game_color=0x7f050006;
+        public static final int home_color=0x7f050000;
+        public static final int movie_color=0x7f050001;
+        public static final int music_color=0x7f050002;
+        public static final int organize_color=0x7f050005;
+    }
     public static final class dimen {
-        /**
-         * Default screen margins, per the Android Design guidelines.
-         * <p/>
-         * Customize dimensions originally defined in res/values/dimens.xml (such as
-         * screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+        /** 
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
          */
-        public static final int activity_horizontal_margin = 0x7f040000;
-        public static final int activity_vertical_margin = 0x7f040001;
+        public static final int activity_horizontal_margin=0x7f040001;
+        /**  Default screen margins, per the Android Design guidelines. 
+         */
+        public static final int home_bar_icon_dim=0x7f040000;
     }
-
     public static final class drawable {
-        public static final int ic_launcher = 0x7f020000;
+        public static final int ic_action_edit=0x7f020000;
+        public static final int ic_action_home=0x7f020001;
+        public static final int ic_action_new=0x7f020002;
+        public static final int ic_action_previous_item=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_action_select_all=0x7f020005;
+        public static final int ic_action_settings=0x7f020006;
+        public static final int ic_action_share=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int ic_nav_block=0x7f020009;
+        public static final int ic_thor=0x7f02000a;
     }
-
     public static final class id {
-        public static final int action_settings = 0x7f080000;
+        public static final int discoverButton=0x7f090003;
+        public static final int homeButton=0x7f090001;
+        public static final int home_action_settings=0x7f090007;
+        public static final int home_bar=0x7f090000;
+        public static final int list_action_add=0x7f090005;
+        public static final int list_action_edit=0x7f090006;
+        public static final int organizeButton=0x7f090002;
+        public static final int organize_action_add=0x7f090008;
+        public static final int pane_action_add=0x7f090009;
+        public static final int searchButton=0x7f090004;
     }
-
     public static final class layout {
-        public static final int activity_main = 0x7f030000;
+        public static final int activity_discover=0x7f030000;
+        public static final int activity_list=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_organize=0x7f030003;
+        public static final int activity_pane=0x7f030004;
+        public static final int activity_search=0x7f030005;
+        public static final int home_bar=0x7f030006;
+        public static final int horizontal_scroll_bar=0x7f030007;
     }
-
     public static final class menu {
-        public static final int main = 0x7f070000;
+        public static final int discover=0x7f080000;
+        public static final int list=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int organize=0x7f080003;
+        public static final int pane=0x7f080004;
+        public static final int search=0x7f080005;
     }
-
     public static final class string {
-        public static final int action_settings = 0x7f050001;
-        public static final int app_name = 0x7f050000;
-        public static final int hello_world = 0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int dis_additional_tab_1=0x7f06001a;
+        public static final int dis_additional_tab_2=0x7f06001b;
+        public static final int dis_additional_tab_3=0x7f06001c;
+        public static final int dis_books_relates_tab_1=0x7f060023;
+        public static final int dis_books_relates_tab_2=0x7f060024;
+        public static final int dis_books_relates_tab_3=0x7f060025;
+        /**  Discover View  
+         */
+        public static final int dis_friends_tab_name=0x7f060018;
+        public static final int dis_games_relates_tab_1=0x7f060020;
+        public static final int dis_games_relates_tab_2=0x7f060021;
+        public static final int dis_games_relates_tab_3=0x7f060022;
+        public static final int dis_media_relates_tab_1=0x7f06001d;
+        public static final int dis_media_relates_tab_2=0x7f06001e;
+        public static final int dis_media_relates_tab_3=0x7f06001f;
+        public static final int dis_popular_tab_name=0x7f060019;
+        public static final int discover_button=0x7f06000a;
+        public static final int discover_view=0x7f06000d;
+        public static final int friends_item_info=0x7f060017;
+        /**  Friends View 
+         */
+        public static final int friends_title=0x7f060016;
+        /** Action Bar constants
+         */
+        public static final int home_action_settings=0x7f060001;
+        public static final int home_added=0x7f06002a;
+        /** Home Bar constants
+         */
+        public static final int home_bar=0x7f060006;
+        public static final int home_button=0x7f060008;
+        public static final int home_friends_recent_act=0x7f060028;
+        public static final int home_my_recent_act=0x7f060027;
+        public static final int home_rated=0x7f060029;
+        public static final int home_reviewed=0x7f06002b;
+        /**  Home View 
+         */
+        public static final int home_title=0x7f060026;
+        /** View Name constants
+         */
+        public static final int home_view=0x7f06000b;
+        public static final int item_actor=0x7f060030;
+        /**  Item View 
+         */
+        public static final int item_add=0x7f06002c;
+        public static final int item_director=0x7f06002f;
+        public static final int item_ok=0x7f060034;
+        public static final int item_rating=0x7f06002e;
+        public static final int item_reviews=0x7f060032;
+        public static final int item_share=0x7f06002d;
+        public static final int item_similar=0x7f060033;
+        public static final int item_tags=0x7f060031;
+        public static final int item_view=0x7f06000f;
+        public static final int list_action_add=0x7f060004;
+        public static final int list_action_edit=0x7f060005;
+        /**  List View 
+         */
+        public static final int list_title=0x7f060035;
+        public static final int list_view=0x7f060011;
+        public static final int movies_num_items=0x7f060015;
+        public static final int movies_recent_movies=0x7f060013;
+        /**  Movies View  
+         */
+        public static final int movies_title=0x7f060012;
+        public static final int movies_your_lists=0x7f060014;
+        /**  Notification View 
+         */
+        public static final int notification_title=0x7f060036;
+        public static final int organize_action_add=0x7f060002;
+        public static final int organize_books=0x7f06003a;
+        public static final int organize_button=0x7f060009;
+        public static final int organize_games=0x7f060039;
+        public static final int organize_movies=0x7f060038;
+        public static final int organize_music=0x7f06003b;
+        public static final int organize_num_items=0x7f06003c;
+        /**  Organize View 
+         */
+        public static final int organize_title=0x7f060037;
+        public static final int organize_view=0x7f06000c;
+        public static final int pane_action_add=0x7f060003;
+        public static final int pane_view=0x7f060010;
+        public static final int search_add=0x7f06003e;
+        public static final int search_button=0x7f060007;
+        /**  Search View 
+         */
+        public static final int search_info=0x7f06003d;
+        public static final int search_view=0x7f06000e;
     }
-
     public static final class style {
-        /**
-         * Base application theme, dependent on API level. This theme is replaced
-         * by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-         * <p/>
-         * <p/>
-         * Theme customizations available in newer API levels can go in
-         * res/values-vXX/styles.xml, while customizations related to
-         * backward-compatibility can go here.
-         * <p/>
-         * <p/>
-         * Base application theme for API 11+. This theme completely replaces
-         * AppBaseTheme from res/values/styles.xml on API 11+ devices.
-         * <p/>
-         * API 11 theme customizations can go here.
-         * <p/>
-         * Base application theme for API 14+. This theme completely replaces
-         * AppBaseTheme from BOTH res/values/styles.xml and
-         * res/values-v11/styles.xml on API 14+ devices.
-         * <p/>
-         * API 14 theme customizations can go here.
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme = 0x7f060000;
-        /**
-         * Application theme.
-         * All customizations that are NOT specific to a particular API-level can go here.
+        public static final int AppBaseTheme=0x7f070000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme = 0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
