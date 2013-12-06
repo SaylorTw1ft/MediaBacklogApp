05bd8c4b6c68f0077a37e68d333ec7c61210074b
liaozihan <liaozihan31@gmail.com>
2013-12-06 13:23:39 -0600

    organize view

--- src/media/backlog/medb/OrganizeActivity.java
+++ src/media/backlog/medb/OrganizeActivity.java
@@ -1,18 +1,74 @@
 package media.backlog.medb;
 
-import android.app.Activity;
+import media.backlog.medb.adpater.TabsPagerAdapter;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 /**
  * Created by Arin on 11/13/13.
+ * 
+ * Update by Zihan on 12/06/13.
+ * 
+ * 
  */
-public class OrganizeActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+public class OrganizeActivity extends FragmentActivity implements ActionBar.TabListener {
+	
+	private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private ActionBar actionBar;
+    // Tab titles
+    private String[] tabs = { "Movies", "Games", "Books", "Music" };
+	
+    @SuppressLint("NewApi")
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organize);
+
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.activity_organize);
+        actionBar = getActionBar();
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+ 
+        viewPager.setAdapter(mAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);       
+ 
+        // Adding Tabs
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+        
+        /**
+         * on swiping the viewpager make respective tab selected
+         * */
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+         
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
+         
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+         
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+        
     }
 
     @Override
@@ -39,4 +95,23 @@ public class OrganizeActivity extends Activity {
 
     public void addList() {
     }
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub        
+		viewPager.setCurrentItem(tab.getPosition());
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
 }
