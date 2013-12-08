29fb087bdb1af1a0ee0c245c190aba132589a65d
Arin Tsikretsis <adt608@hotmail.com>
2013-12-08 13:42:37 -0600

    asda

--- src/media/backlog/medb/HorizontalScrollBar.java
+++ src/media/backlog/medb/HorizontalScrollBar.java
@@ -1,10 +1,16 @@
 package media.backlog.medb;
 
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by Arin on 12/2/13.
@@ -12,6 +18,21 @@ import android.view.ViewGroup;
 public class HorizontalScrollBar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        View v = inflater.inflate(R.layout.horizontal_scroll_bar, container, false);
+        LinearLayout l = (LinearLayout) v.findViewById(R.id.horizontal_scroll_bar);
+        TextView t = new TextView(v.getContext());
+        t.setText("  ");
+        l.addView(t);
+        for (int i = 0; i < 9; i++) {
+            ImageButton b = new ImageButton(v.getContext());
+            b.setBackgroundResource(R.drawable.ic_thor);
+            l.addView(b);
+            TextView t1 = new TextView(v.getContext());
+            t1.setText("  ");
+            l.addView(t1);
+        }
+
+        return v;
     }
+
 }
