3486d0c074d9c37411d82d7791ba9e3bfb12d063
Arin Tsikretsis <adt608@hotmail.com>
2013-12-08 16:34:58 -0600

    Fixed up home page and Item Page

---
+++ src/media/backlog/medb/ItemActivity.java
@@ -0,0 +1,13 @@
+package media.backlog.medb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Arin on 12/8/13.
+ */
+public class ItemActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
