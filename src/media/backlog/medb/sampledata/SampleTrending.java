23707e13f324530fac5fca1f67941f4d511617a5
mustafafhd <mustafafhd@gmail.com>
2013-12-08 16:32:11 -0600

    Added SampleTrending class

---
+++ src/media/backlog/medb/sampledata/SampleTrending.java
@@ -0,0 +1,57 @@
+package media.backlog.medb.sampledata;
+
+public class SampleTrending {
+
+	//inception 0
+	String trend1 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,1)";
+	String trend2 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,2)";
+	String trend3 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,3)";
+	String trend4 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,4)";
+	String trend5 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,5)";
+	String trend6 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,6)";
+	String trend7 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,7)";
+	String trend8 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (0,8)";
+	
+	//gta V 14
+	String trend9 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,1)";
+	String trend10 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,2)";
+	String trend11 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,3)";
+	String trend12 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,4)";
+	String trend13 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,5)";
+	String trend14 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,6)";
+	String trend15 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (14,7)";
+	
+	//book catc-22 28
+	String trend16 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (28,1)";
+	String trend17 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (28,2)";
+	String trend18 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (28,3)";
+	String trend19 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (28,4)";
+	String trend20 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (28,5)";
+	String trend21 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (28,6)";
+	
+	//thor 1
+	String trend22 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (1,1)";
+	String trend23 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (1,2)";
+	String trend24 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (1,3)";
+	String trend25 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (1,4)";
+	String trend26 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (1,5)";
+	
+	//Battlefield 4 16
+	String trend27 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (16,1)";
+	String trend28 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (16,2)";
+	String trend29 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (16,3)";
+	String trend30 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (16,4)";
+
+	//the great gatsby 5
+	String trend31 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (5,1)";
+	String trend32 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (5,2)";
+	String trend33 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (5,3)";
+
+	//zelda 15
+	String trend34 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (15,1)";
+	String trend35 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (15,2)";
+
+	//Book the hunger games 35
+	String trend36 = "INSERT INTO TrendingAmongFriends (itemID, friendID) VALUES (35,1)";
+
+}
