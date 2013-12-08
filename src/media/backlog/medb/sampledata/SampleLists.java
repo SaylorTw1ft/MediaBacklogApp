4544e6b37968b80af0f8c313d6cb03351477fc83
mustafafhd <mustafafhd@gmail.com>
2013-12-08 14:36:13 -0600

    Added SampleLists class

---
+++ src/media/backlog/medb/sampledata/SampleLists.java
@@ -0,0 +1,20 @@
+package media.backlog.medb.sampledata;
+
+public class SampleLists {
+
+	String list1 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (1, 'Stuff to Buy',1,1,1,0)";
+	String list2 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (2, 'Movies I\'ve Seen',1,0,0,0)";
+	String list3 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (3, 'Games I\'ve Played',0,1,0,0)";
+	String list4 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (4, 'Books I\'ve Read',0,0,0,1)";
+	String list5 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (5, 'Albums I\'ve listened to',0,0,1,0)";
+	String list6 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (6, 'My favorite movies',1,0,0,0)";
+	String list7 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (7, 'Batman',1,1,0,1)";
+	String list8 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (8, 'Games I Want to Play',0,1,0,0)";
+	String list9 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (9, 'Science Fiction',1,1,0,1)";
+	String list10 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (10, 'Xbox Games',0,1,0,0)";
+	String list11 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (11, 'Gifts',1,1,1,1)";
+	String list12 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (12, 'Books I Liked',0,0,0,1)";
+	String list13 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (13, 'The Hunger Games',1,0,0,1)";
+	String list14 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (14, 'Books I want to buy',0,0,0,1)";
+	String list15 = "INSERT INTO Lists(listID,itemName,movie,game,music,book) VALUE (15, 'Favorite Albums',0,0,1,0)";
+}
