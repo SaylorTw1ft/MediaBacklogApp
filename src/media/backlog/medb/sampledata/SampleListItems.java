eee8a104f0a227cda6229e3c452ba7ebc4bde5bc
mustafafhd <mustafafhd@gmail.com>
2013-12-08 16:13:50 -0600

    addeed public static to SampleFriends and SampleListItem

--- src/media/backlog/medb/sampledata/SampleListItems.java
+++ src/media/backlog/medb/sampledata/SampleListItems.java
@@ -6,79 +6,79 @@ public class SampleListItems {
 	//asdfg
 	
 	//Stuff to Buy
-	String listItem4 = "INSERT INTO ListItems (listID, itemID) VALUES (1,1)";
-	String listItem1 = "INSERT INTO ListItems (listID, itemID) VALUES (1,18)";
-	String listItem2 = "INSERT INTO ListItems (listID, itemID) VALUES (1,28)";
-	String listItem3 = "INSERT INTO ListItems (listID, itemID) VALUES (1,39)";
+	public static String listItem4 = "INSERT INTO ListItems (listID, itemID) VALUES (1,1)";
+	public static String listItem1 = "INSERT INTO ListItems (listID, itemID) VALUES (1,18)";
+	public static String listItem2 = "INSERT INTO ListItems (listID, itemID) VALUES (1,28)";
+	public static String listItem3 = "INSERT INTO ListItems (listID, itemID) VALUES (1,39)";
 	
 	//Movies I've seen
-	String listItem5 = "INSERT INTO ListItems (listID, itemID) VALUES (2,1)";
-	String listItem6 = "INSERT INTO ListItems (listID, itemID) VALUES (2,2)";
-	String listItem7 = "INSERT INTO ListItems (listID, itemID) VALUES (2,3)";
-	String listItem8 = "INSERT INTO ListItems (listID, itemID) VALUES (2,4)";
+	public static String listItem5 = "INSERT INTO ListItems (listID, itemID) VALUES (2,1)";
+	public static String listItem6 = "INSERT INTO ListItems (listID, itemID) VALUES (2,2)";
+	public static String listItem7 = "INSERT INTO ListItems (listID, itemID) VALUES (2,3)";
+	public static String listItem8 = "INSERT INTO ListItems (listID, itemID) VALUES (2,4)";
 	
 	//Games I've Played
-	String listItem9 = "INSERT INTO ListItems (listID, itemID) VALUES (3,14)";
-	String listItem10 = "INSERT INTO ListItems (listID, itemID) VALUES (3,15)";
-	String listItem11 = "INSERT INTO ListItems (listID, itemID) VALUES (3,16)";
-	String listItem12 = "INSERT INTO ListItems (listID, itemID) VALUES (3,17)";
+	public static String listItem9 = "INSERT INTO ListItems (listID, itemID) VALUES (3,14)";
+	public static String listItem10 = "INSERT INTO ListItems (listID, itemID) VALUES (3,15)";
+	public static String listItem11 = "INSERT INTO ListItems (listID, itemID) VALUES (3,16)";
+	public static String listItem12 = "INSERT INTO ListItems (listID, itemID) VALUES (3,17)";
 
 	//Books I've Read
-	String listItem13 = "INSERT INTO ListItems (listID, itemID) VALUES (4,27)";
-	String listItem14 = "INSERT INTO ListItems (listID, itemID) VALUES (4,29)";
-	String listItem15 = "INSERT INTO ListItems (listID, itemID) VALUES (4,30)";
+	public static String listItem13 = "INSERT INTO ListItems (listID, itemID) VALUES (4,27)";
+	public static String listItem14 = "INSERT INTO ListItems (listID, itemID) VALUES (4,29)";
+	public static String listItem15 = "INSERT INTO ListItems (listID, itemID) VALUES (4,30)";
 	
 	//Albums I've Listened to
-	String listItem16 = "INSERT INTO ListItems (listID, itemID) VALUES (5,40)";
-	String listItem17 = "INSERT INTO ListItems (listID, itemID) VALUES (5,41)";
+	public static String listItem16 = "INSERT INTO ListItems (listID, itemID) VALUES (5,40)";
+	public static String listItem17 = "INSERT INTO ListItems (listID, itemID) VALUES (5,41)";
 	
 	//My Favorite movies
-	String listItem18 = "INSERT INTO ListItems (listID, itemID) VALUES (6,5)";
-	String listItem19 = "INSERT INTO ListItems (listID, itemID) VALUES (6,6)";
-	String listItem20 = "INSERT INTO ListItems (listID, itemID) VALUES (6,7)";
+	public static String listItem18 = "INSERT INTO ListItems (listID, itemID) VALUES (6,5)";
+	public static String listItem19 = "INSERT INTO ListItems (listID, itemID) VALUES (6,6)";
+	public static String listItem20 = "INSERT INTO ListItems (listID, itemID) VALUES (6,7)";
 	
 	//Batman
-	String listItem21 = "INSERT INTO ListItems (listID, itemID) VALUES (7,2)";
+	public static String listItem21 = "INSERT INTO ListItems (listID, itemID) VALUES (7,2)";
 	//String listItem22 = "INSERT INTO ListItems (listID, itemID) VALUES (7,)";
-	String listItem23 = "INSERT INTO ListItems (listID, itemID) VALUES (7,34)";
+	public static String listItem23 = "INSERT INTO ListItems (listID, itemID) VALUES (7,34)";
 	
 	//Games I want to Play
-	String listItem24 = "INSERT INTO ListItems (listID, itemID) VALUES (8,19)";
-	String listItem25 = "INSERT INTO ListItems (listID, itemID) VALUES (8,20)";
-	String listItem26 = "INSERT INTO ListItems (listID, itemID) VALUES (8,21)";
+	public static String listItem24 = "INSERT INTO ListItems (listID, itemID) VALUES (8,19)";
+	public static String listItem25 = "INSERT INTO ListItems (listID, itemID) VALUES (8,20)";
+	public static String listItem26 = "INSERT INTO ListItems (listID, itemID) VALUES (8,21)";
 	
 	//Science Fiction
-	String listItem27 = "INSERT INTO ListItems (listID, itemID) VALUES (9,4)";
+	public static String listItem27 = "INSERT INTO ListItems (listID, itemID) VALUES (9,4)";
 	//String listItem28 = "INSERT INTO ListItems (listID, itemID) VALUES (9,)";
 	//String listItem29 = "INSERT INTO ListItems (listID, itemID) VALUES (9,)";
 	
 	//Xbox Games
-	String listItem30 = "INSERT INTO ListItems (listID, itemID) VALUES (10,14)";
-	String listItem31 = "INSERT INTO ListItems (listID, itemID) VALUES (10,16)";
-	String listItem32 = "INSERT INTO ListItems (listID, itemID) VALUES (10,17)";
-	String listItem33 = "INSERT INTO ListItems (listID, itemID) VALUES (10,24)";
+	public static String listItem30 = "INSERT INTO ListItems (listID, itemID) VALUES (10,14)";
+	public static String listItem31 = "INSERT INTO ListItems (listID, itemID) VALUES (10,16)";
+	public static String listItem32 = "INSERT INTO ListItems (listID, itemID) VALUES (10,17)";
+	public static String listItem33 = "INSERT INTO ListItems (listID, itemID) VALUES (10,24)";
 	
 	//Gifts
-	String listItem34 = "INSERT INTO ListItems (listID, itemID) VALUES (11,8)";
-	String listItem35 = "INSERT INTO ListItems (listID, itemID) VALUES (11,22)";
-	String listItem36 = "INSERT INTO ListItems (listID, itemID) VALUES (11,37)";
-	String listItem37 = "INSERT INTO ListItems (listID, itemID) VALUES (11,42)";
+	public static String listItem34 = "INSERT INTO ListItems (listID, itemID) VALUES (11,8)";
+	public static String listItem35 = "INSERT INTO ListItems (listID, itemID) VALUES (11,22)";
+	public static String listItem36 = "INSERT INTO ListItems (listID, itemID) VALUES (11,37)";
+	public static String listItem37 = "INSERT INTO ListItems (listID, itemID) VALUES (11,42)";
 	
 	//Books I Liked
-	String listItem38 = "INSERT INTO ListItems (listID, itemID) VALUES (12,35)";
-	String listItem39 = "INSERT INTO ListItems (listID, itemID) VALUES (12,29)";
+	public static String listItem38 = "INSERT INTO ListItems (listID, itemID) VALUES (12,35)";
+	public static String listItem39 = "INSERT INTO ListItems (listID, itemID) VALUES (12,29)";
 	
 	//The Hunger Games
-	String listItem40 = "INSERT INTO ListItems (listID, itemID) VALUES (13,38)";
-	String listItem41 = "INSERT INTO ListItems (listID, itemID) VALUES (13,35)";
-	String listItem42 = "INSERT INTO ListItems (listID, itemID) VALUES (13,43)";
+	public static String listItem40 = "INSERT INTO ListItems (listID, itemID) VALUES (13,38)";
+	public static String listItem41 = "INSERT INTO ListItems (listID, itemID) VALUES (13,35)";
+	public static String listItem42 = "INSERT INTO ListItems (listID, itemID) VALUES (13,43)";
 	//Books I want to buy
-	String listItem43 = "INSERT INTO ListItems (listID, itemID) VALUES (14,30)";
-	String listItem44 = "INSERT INTO ListItems (listID, itemID) VALUES (14,36)";
-	String listItem45 = "INSERT INTO ListItems (listID, itemID) VALUES (14,32)";
+	public static String listItem43 = "INSERT INTO ListItems (listID, itemID) VALUES (14,30)";
+	public static String listItem44 = "INSERT INTO ListItems (listID, itemID) VALUES (14,36)";
+	public static String listItem45 = "INSERT INTO ListItems (listID, itemID) VALUES (14,32)";
 	//Favorite Albums
-	String listItem46 = "INSERT INTO ListItems (listID, itemID) VALUES (15,40)";
-	String listItem47 = "INSERT INTO ListItems (listID, itemID) VALUES (15,41)";
-	String listItem48 = "INSERT INTO ListItems (listID, itemID) VALUES (15,44)";
+	public static String listItem46 = "INSERT INTO ListItems (listID, itemID) VALUES (15,40)";
+	public static String listItem47 = "INSERT INTO ListItems (listID, itemID) VALUES (15,41)";
+	public static String listItem48 = "INSERT INTO ListItems (listID, itemID) VALUES (15,44)";
 
 }
