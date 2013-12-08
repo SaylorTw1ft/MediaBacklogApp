bb4644a378668337dbddc85e1664bc3c2f87b641
mustafafhd <mustafafhd@gmail.com>
2013-12-08 15:46:05 -0600

    added SampleFriends class

---
+++ src/media/backlog/medb/sampledata/SampleFriends.java
@@ -0,0 +1,21 @@
+package media.backlog.medb.sampledata;
+
+public class SampleFriends {
+	String friend1 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (1,'Arin','friends/arin.png')";
+	String friend2 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (2,'Danny','friends/danny.png')";
+	String friend3 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (3,'Karan','friends/karan.png')";
+	String friend4 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (4,'Fahad','friends/fahad.png')";
+	String friend5 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (5,'Zihan','friends/zihan.png')";
+	String friend6 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (6,'Hao','friends/hao.png')";
+	String friend7 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (7,'Pratham','friends/pratham.png')";
+	String friend8 = "INSERT INTO Friends (friendID, friendName, picture) " +
+			"VALUES (8,'Shobhit','friends/shobhit.png')";
+
+}
