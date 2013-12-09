bba7b81d97080e7a3650a33ce5da0738eff30388
Arin Tsikretsis <adt608@hotmail.com>
2013-12-09 14:05:03 -0600

    fixing conflicts

--- src/media/backlog/medb/HorizontalScrollBar.java
+++ src/media/backlog/medb/HorizontalScrollBar.java
@@ -1,16 +1,18 @@
 package media.backlog.medb;
 
 import android.app.Fragment;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableContainer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+
+import media.backlog.medb.data.MediaList;
+import media.backlog.medb.data.MediaItem;
+import media.backlog.medb.database.DatabaseHelper;
 
 /**
  * Created by Arin on 12/2/13.
@@ -23,6 +25,10 @@ public class HorizontalScrollBar extends Fragment {
         TextView t = new TextView(v.getContext());
         t.setText("  ");
         l.addView(t);
+//        DatabaseHelper helper = new DatabaseHelper(getActivity().getApplicationContext());
+//        Bundle bundle = new Bundle();
+//        int listId = bundle.getInt("listId");
+//        MediaList itemList = media.backlog.medb.database.Lists.getList(helper, listId);
         for (int i = 0; i < 9; i++) {
             ImageButton b = new ImageButton(v.getContext());
             b.setBackgroundResource(R.drawable.ic_thor);
