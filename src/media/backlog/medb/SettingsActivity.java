9672a52f76bb0127d25892c9b55972ee2c7338e8
Arin Tsikretsis <adt608@hotmail.com>
2013-12-09 14:08:11 -0600

    added new activities

---
+++ src/media/backlog/medb/SettingsActivity.java
@@ -0,0 +1,14 @@
+package media.backlog.medb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Arin on 12/9/13.
+ */
+public class SettingsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
