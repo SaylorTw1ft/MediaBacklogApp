42c47f7c7999004566559e0335f7e9e5c4656dfa
mustafafhd <mustafafhd@gmail.com>
2013-12-09 15:07:06 -0600

    Up button on settings page

--- src/media/backlog/medb/SettingsActivity.java
+++ src/media/backlog/medb/SettingsActivity.java
@@ -10,5 +10,6 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
