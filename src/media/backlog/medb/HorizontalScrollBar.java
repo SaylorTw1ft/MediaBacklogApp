f6c885f52084ee11cd5b0495d3912d21731a9668
Arin Tsikretsis <adt608@hotmail.com>
2013-12-09 17:30:26 -0600

    Horizontal Scroll

--- src/media/backlog/medb/HorizontalScrollBar.java
+++ src/media/backlog/medb/HorizontalScrollBar.java
@@ -2,15 +2,24 @@ package media.backlog.medb;
 
 import android.app.Fragment;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ImageView.ScaleType;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import media.backlog.medb.data.MediaItem;
+import media.backlog.medb.database.DatabaseHelper;
 
 /**
  * Created by Arin on 12/2/13.
@@ -24,24 +33,68 @@ public class HorizontalScrollBar extends Fragment
         TextView t = new TextView(v.getContext());
         t.setText("  ");
         l.addView(t);
+        DatabaseHelper helper = new DatabaseHelper(getActivity().getApplicationContext());
+        Bundle bundle = new Bundle();
+        int id = 1;
+        MediaItem item = media.backlog.medb.database.Items.getItem(helper, id);
+        ArrayList<MediaItem> items =  media.backlog.medb.database.Items.getSimilarItems(helper, item);
 
-        for (int i = 0; i < 9; i++)
-        {
-            LinearLayout scrollingItem = new LinearLayout(v.getContext());
-        	scrollingItem.setOrientation(LinearLayout.VERTICAL);
-            ImageButton b = new ImageButton(v.getContext());
-            b.setBackgroundResource(R.drawable.ic_thor);
-            scrollingItem.addView(b);
-            TextView t1 = new TextView(v.getContext());
-            t1.setText("Thor");
-            t1.setTextColor(Color.WHITE);
-            t1.setGravity(Gravity.CENTER_HORIZONTAL);
-            scrollingItem.addView(t1);
-            
+        for (int i = 0; i < 10 && i<items.size(); i++) {
+            MediaItem temp = items.get(i);
+            LinearLayout scrollingItem = setUpScrollingItem(v, temp);
+
             l.addView(scrollingItem);
+
+            TextView buffer = new TextView(v.getContext());
+            buffer.setText("  ");
+            l.addView(buffer);
         }
 
         return v;
+    }
+
+    private LinearLayout setUpScrollingItem(View v, MediaItem temp) {
+        LinearLayout scrollingItem = new LinearLayout(v.getContext());
+        scrollingItem.setLayoutParams(new LinearLayout.LayoutParams(188,250));
+        scrollingItem.setOrientation(LinearLayout.VERTICAL);
+        ImageButton button = new ImageButton(scrollingItem.getContext());
+        button.setLayoutParams(new ViewGroup.LayoutParams(150,200));
+        button.setScaleType(ScaleType.FIT_START);
+        setThumbnailPic(temp, button);
+        TextView t1 = new TextView(v.getContext());
+        t1.setText(temp.getItemName());
+        t1.setTextColor(Color.WHITE);
+        t1.setTextSize(12);
+        t1.setGravity(Gravity.BOTTOM);
+        scrollingItem.setMinimumHeight(200);
+        scrollingItem.setMinimumWidth(134);
+        scrollingItem.addView(button);
+        scrollingItem.addView(t1);
+        return scrollingItem;
+    }
+
+    public void setThumbnailPic(MediaItem item, ImageButton imageButton)
+    {
+        String path = item.getPicture();
+        try {
+
+            // get input stream
+
+            InputStream ims = getActivity().getAssets().open(path);
+
+            // load image as Drawable
+
+            Drawable d = Drawable.createFromStream(ims, null);
+
+            // set image to ImageView
+            imageButton.setImageDrawable(d);
+
+        }
+
+        catch(IOException ex) {
+
+
+        }
     }
 
 }
