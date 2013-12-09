478d62b401ab00ebaf798e37dd0e038ae00d7a82
Danny Briggs <briggs11@illinois.edu>
2013-12-09 13:17:41 -0600

    The invisible buttons on home screen are now clickable

--- src/media/backlog/medb/MainActivity.java
+++ src/media/backlog/medb/MainActivity.java
@@ -2,8 +2,12 @@ package media.backlog.medb;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +15,51 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button movie = (Button) findViewById(R.id.movie_button);
+        movie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrganizeActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("category", 1);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        Button game = (Button) findViewById(R.id.game_button);
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrganizeActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("category", 2);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        Button book = (Button) findViewById(R.id.book_button);
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrganizeActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("category", 3);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        Button music = (Button) findViewById(R.id.music_button);
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrganizeActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("category", 4);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -36,4 +85,27 @@ public class MainActivity extends Activity {
     public void openSettings() {
     }
 
+    /*
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+    	float eventX = event.getX();
+    	float eventY = event.getY();
+
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN:{
+        	
+        }
+        case MotionEvent.ACTION_POINTER_DOWN:
+        case MotionEvent.ACTION_MOVE:
+        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_POINTER_UP:
+        case MotionEvent.ACTION_CANCEL: {
+          break;
+        }
+        }
+
+        return true;
+    } 
+    */
 }
