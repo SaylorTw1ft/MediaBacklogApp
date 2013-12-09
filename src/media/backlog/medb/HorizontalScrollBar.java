d1cc56179b67f06fc983d0354ea875830e27b328
SaylorTw1ft <kbatra@live.com>
2013-12-09 14:43:53 -0600

    scrolling item

--- src/media/backlog/medb/HorizontalScrollBar.java
+++ src/media/backlog/medb/HorizontalScrollBar.java
@@ -10,14 +10,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
-import media.backlog.medb.data.MediaList;
-import media.backlog.medb.data.MediaItem;
-import media.backlog.medb.database.DatabaseHelper;
-
 /**
  * Created by Arin on 12/2/13.
  */
-public class HorizontalScrollBar extends Fragment {
+public class HorizontalScrollBar extends Fragment
+{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.horizontal_scroll_bar, container, false);
@@ -25,17 +22,19 @@ public class HorizontalScrollBar extends Fragment {
         TextView t = new TextView(v.getContext());
         t.setText("  ");
         l.addView(t);
-//        DatabaseHelper helper = new DatabaseHelper(getActivity().getApplicationContext());
-//        Bundle bundle = new Bundle();
-//        int listId = bundle.getInt("listId");
-//        MediaList itemList = media.backlog.medb.database.Lists.getList(helper, listId);
-        for (int i = 0; i < 9; i++) {
+
+        for (int i = 0; i < 9; i++)
+        {
+            LinearLayout scrollingItem = new LinearLayout(v.getContext());
+        	scrollingItem.setOrientation(LinearLayout.VERTICAL);
             ImageButton b = new ImageButton(v.getContext());
             b.setBackgroundResource(R.drawable.ic_thor);
-            l.addView(b);
+            scrollingItem.addView(b);
             TextView t1 = new TextView(v.getContext());
-            t1.setText("  ");
-            l.addView(t1);
+            t1.setText("Thor");
+            scrollingItem.addView(t1);
+            
+            l.addView(scrollingItem);
         }
 
         return v;
