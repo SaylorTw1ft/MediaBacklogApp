a2363ba1472a1a61641b06f09c304da93905a54a
SaylorTw1ft <kbatra@live.com>
2013-12-08 21:41:22 -0600

    Fixed database errors

--- src/media/backlog/medb/LoginScreen.java
+++ src/media/backlog/medb/LoginScreen.java
@@ -3,6 +3,7 @@ package media.backlog.medb;
 import media.backlog.medb.database.DatabaseHelper;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,8 @@ public class LoginScreen extends Activity
     {
         super.onCreate(savedInstanceState);
         DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
-        dbHelper.getWritableDatabase();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        dbHelper.loadSampleData(db);
         setContentView(R.layout.activity_login);
         
 		
--- src/media/backlog/medb/sampledata/SampleLists.java
+++ src/media/backlog/medb/sampledata/SampleLists.java
@@ -2,19 +2,19 @@ package media.backlog.medb.sampledata;
 
 public class SampleLists
 {
-	public static String list1 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (1, 'Stuff to Buy',1,1,1,0)";
-	public static String list2 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (2, 'Movies I\'ve Seen',1,0,0,0)";
-	public static String list3 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (3, 'Games I\'ve Played',0,1,0,0)";
-	public static String list4 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (4, 'Books I\'ve Read',0,0,0,1)";
-	public static String list5 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (5, 'Albums I\'ve listened to',0,0,1,0)";
-	public static String list6 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (6, 'My favorite movies',1,0,0,0)";
-	public static String list7 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (7, 'Batman',1,1,0,1)";
-	public static String list8 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (8, 'Games I Want to Play',0,1,0,0)";
-	public static String list9 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (9, 'Science Fiction',1,1,0,1)";
-	public static String list10 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (10, 'Xbox Games',0,1,0,0)";
-	public static String list11 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (11, 'Gifts',1,1,1,1)";
-	public static String list12 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (12, 'Books I Liked',0,0,0,1)";
-	public static String list13 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (13, 'The Hunger Games',1,0,0,1)";
-	public static String list14 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (14, 'Books I want to buy',0,0,0,1)";
-	public static String list15 = "INSERT INTO Lists (listID,itemName,movie,game,music,book) VALUES (15, 'Favorite Albums',0,0,1,0)";
+	public static String list1 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (1, 'Stuff to Buy',1,1,1,0)";
+	public static String list2 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (2, 'Movies Ive Seen',1,0,0,0)";
+	public static String list3 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (3, 'Games Ive Played',0,1,0,0)";
+	public static String list4 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (4, 'Books Ive Read',0,0,0,1)";
+	public static String list5 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (5, 'Albums Ive listened to',0,0,1,0)";
+	public static String list6 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (6, 'My favorite movies',1,0,0,0)";
+	public static String list7 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (7, 'Batman',1,1,0,1)";
+	public static String list8 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (8, 'Games I Want to Play',0,1,0,0)";
+	public static String list9 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (9, 'Science Fiction',1,1,0,1)";
+	public static String list10 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (10, 'Xbox Games',0,1,0,0)";
+	public static String list11 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (11, 'Gifts',1,1,1,1)";
+	public static String list12 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (12, 'Books I Liked',0,0,0,1)";
+	public static String list13 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (13, 'The Hunger Games',1,0,0,1)";
+	public static String list14 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (14, 'Books I want to buy',0,0,0,1)";
+	public static String list15 = "INSERT INTO Lists (listID,listName,movie,game,music,book) VALUES (15, 'Favorite Albums',0,0,1,0)";
 }
