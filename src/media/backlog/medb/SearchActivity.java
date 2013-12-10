a77fe0bd25531de5d7b7b2e751e4f7523bf9dfe4
Chen Hao <Chen Hao@ChenHao-HP>
2013-12-10 11:31:44 +0800

    search function

--- src/media/backlog/medb/SearchActivity.java
+++ src/media/backlog/medb/SearchActivity.java
@@ -2,10 +2,13 @@ package media.backlog.medb;
 
 import java.util.ArrayList;
 
+import media.backlog.medb.adapter.OrgListAdapter;
 import media.backlog.medb.adapter.SearchResultListAdaptor;
 import media.backlog.medb.data.MediaItem;
+import media.backlog.medb.data.MediaList;
 import media.backlog.medb.database.DatabaseHelper;
 import media.backlog.medb.database.Items;
+import media.backlog.medb.database.Lists;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
@@ -15,9 +18,14 @@ import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
+import android.widget.ToggleButton;
 
 /**
  * Created by Arin on 11/13/13.
@@ -27,46 +35,102 @@ import android.widget.SearchView;
 
 
 public class SearchActivity extends Activity {
-	SearchResultListAdaptor adptor;
+
+	SearchResultListAdaptor adapter;
+	ArrayList<MediaItem> all_Item;
+	ListView search_list;
+	RelativeLayout r;
+	Button search_BUT;
+	Button cancel_BUT;
+	DatabaseHelper dbHelper;
+
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_list);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_box);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		search_BUT = (Button)findViewById(R.id.search_BUTTON);
+		cancel_BUT = (Button)findViewById(R.id.cancel_BUTTON);
+		search_BUT.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				handleIntent(getIntent());
+			
+			}
 
-	    // Get the intent, verify the action and get the query
-	    Intent intent = getIntent();
-	    ListView la;
-	   
-	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	      String query = intent.getStringExtra(SearchManager.QUERY);
-	      
-	      //db connection
-	      DatabaseHelper meDbHelper = new DatabaseHelper(getApplicationContext());
-	      ArrayList<MediaItem> searchResults = Items.getSearchResults(meDbHelper, query);
-	      
-	      adptor = new SearchResultListAdaptor(this,R.layout.search_list_display,searchResults);
-	      la = (ListView)findViewById(R.id.org_list);
-	      la.setAdapter(adptor);
-	    }
+		});	
+		
+		cancel_BUT.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.search_box);
+			}
+
+		});
+
+
+
+		
 	}
-	
 	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.search, menu);
-        return true;
-    }
+	protected void onNewIntent(Intent intent) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
+		handleIntent(intent);
+	}
+	public void onListItemClick(ListView l, 
+			View v, int position, long id) { 
+		// call detail activity for clicked entry 
+	}
 
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	private void handleIntent(Intent intent) {
+
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			//use the query to search your data somehow
+			doSearch(query);
+		}
+	}
+	private void doSearch(String queryStr) { 
+		// get a Cursor, prepare the ListAdapter
+		// and set it
+		dbHelper = new DatabaseHelper(getApplicationContext());
+		all_Item=Items.getSearchResults(dbHelper, queryStr);
+		r = (RelativeLayout) findViewById(R.id.org_layout);
+
+		search_list = (ListView) findViewById(R.id.org_list);
+		adapter = new SearchResultListAdaptor(this, R.layout.list_item, all_Item);
+		search_list.setAdapter(adapter);
+	} 
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+
+		// Associate searchable configuration with the SearchView
+		SearchManager searchManager =
+				(SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView =
+				(SearchView) menu.findItem(R.id.search).getActionView();
+		searchView.setSearchableInfo(
+				searchManager.getSearchableInfo(getComponentName()));
+
+		return true;
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
