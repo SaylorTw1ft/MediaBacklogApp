3eae2bc5b157819d18fc505b3f929d80ae65dd22
Danny Briggs <briggs11@illinois.edu>
2013-12-10 02:50:17 -0600

    Changed the color of each fragment depending on whether your in the movies/games/etc.. page.

--- src/media/backlog/medb/PaneActivity.java
+++ src/media/backlog/medb/PaneActivity.java
@@ -1,28 +1,58 @@
 package media.backlog.medb;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Arin on 11/28/13.
  */
 public class PaneActivity extends Activity {
 	
+	public void setHomeBarColor(int color)
+	{
+        Fragment homeFrag = getFragmentManager().findFragmentById(R.id.home_bar);
+        View mainView = homeFrag.getView();
+        View homeView = mainView.findViewById(R.id.homeButton);
+        View organizeView = mainView.findViewById(R.id.organizeButton);
+        View discoverView = mainView.findViewById(R.id.discoverButton);
+        View searchView = mainView.findViewById(R.id.searchButton);
+        
+        mainView.setBackgroundColor(color);
+        homeView.setBackgroundColor(color);
+        organizeView.setBackgroundColor(color);
+        discoverView.setBackgroundColor(color);
+        searchView.setBackgroundColor(color);
+	}
 	public void setUpPage(int category){
-        //Movies
+		//Movies
         if (category == 1)
+        {
         	setTitle("Movies");
+            setHomeBarColor(getResources().getColor(R.color.movie_color));
+        }
         //Games
         if(category == 2)
+        {
         	setTitle("Games");
-        //Books
-        if(category == 3)
-        	setTitle("Music");
+        	setHomeBarColor(getResources().getColor(R.color.game_color));
+        }
         //Music
+        if(category == 3)
+        {
+        	setTitle("Music");
+        	setHomeBarColor(getResources().getColor(R.color.music_color));
+        }
+        //Books
         if(category == 4)
+        {
         	setTitle("Books");
+        	setHomeBarColor(getResources().getColor(R.color.book_color));
+        }
 	}
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
