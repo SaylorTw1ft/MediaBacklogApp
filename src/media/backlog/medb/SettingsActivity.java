0d67b28187f07d52490504e6d871ebc38d2351af
mustafafhd <mustafafhd@gmail.com>
2013-12-10 15:23:55 -0600

    Implemented log out

--- src/media/backlog/medb/SettingsActivity.java
+++ src/media/backlog/medb/SettingsActivity.java
@@ -1,10 +1,13 @@
 package media.backlog.medb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Arin on 12/9/13.
@@ -14,6 +17,17 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        Button logout = (Button) findViewById(R.id.log_out_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(SettingsActivity.this, LoginScreen.class);
+				intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);				
+			}
+		});
     }
     
     @Override
