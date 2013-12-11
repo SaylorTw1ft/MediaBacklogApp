95fa38f6c13474d30fca3951dfb59a685edcff32
Danny Briggs <briggs11@illinois.edu>
2013-12-11 03:43:00 -0600

    Fixed the item page further.

--- src/media/backlog/medb/ItemActivity.java
+++ src/media/backlog/medb/ItemActivity.java
@@ -6,43 +6,104 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageView.ScaleType;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import media.backlog.medb.database.DatabaseHelper;
+import media.backlog.medb.database.Items;
 import media.backlog.medb.data.MediaItem;
 
 /**
  * Created by Arin on 12/8/13.
  */
 public class ItemActivity extends Activity {
+	private static int staticCategory = 1;
     private MediaItem item;
     private DatabaseHelper helper;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
         Bundle b = getIntent().getExtras();
         int id = b.getInt("id");
-        setContentView(R.layout.activity_item);
-        helper = new DatabaseHelper(getApplicationContext());
-        item = media.backlog.medb.database.Items.getItem(helper, id);
+		DatabaseHelper helper = new DatabaseHelper(getApplicationContext());
+		item = media.backlog.medb.database.Items.getItem(helper, id);
+        int category;
+        
+        try
+        {
+        	category = b.getInt("category");
+        	staticCategory = category;
+        }
+        catch(Exception e)
+        {
+        	category = staticCategory;
+        }
+
+        setTitle(item.getItemName());
         setItemName();
-        setTitleName();
         setItemGenre();
         setUpButtons();
         setItemThumbnail();
+
+        //setContentView(R.layout.activity_item);
+        		
+		WhiteHorizontalScrollBar scrollBar = (WhiteHorizontalScrollBar)
+        getFragmentManager().findFragmentById(R.id.horizontal_scroll_bar_item);
+		
+        if(scrollBar != null)
+        {
+        	HorizontalScrollView scrollView = (HorizontalScrollView) scrollBar.getView();
+        	LinearLayout view1 = (LinearLayout) scrollView.findViewById(R.id.horizontal_scroll_bar);
+        	
+        	if(view1 != null)
+        	{
+        		view1.removeAllViews();
+        		//view1.removeAllViewsInLayout();
+        		
+                TextView t = new TextView(view1.getContext());
+                t.setText("  ");
+                view1.addView(t);
+                
+                ArrayList<MediaItem> paneItems = Items.getSimilarItems(helper, item);
+
+                for (int i = 0; i < paneItems.size(); i++)
+                {
+                    MediaItem item1 = paneItems.get(i);
+                    LinearLayout scrollingItem = setUpScrollingItem(view1, item1);
+                    view1.addView(scrollingItem);
+
+                    TextView buffer = new TextView(view1.getContext());
+                    buffer.setText("  ");
+                    view1.addView(buffer);
+                }
+        	}
+        }
+		
+/*
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Bundle args = new Bundle();
@@ -50,11 +111,15 @@ public class ItemActivity extends Activity {
         WhiteHorizontalScrollBar horizontalScrollBar = new WhiteHorizontalScrollBar();
         horizontalScrollBar.setArguments(args);
         fragmentTransaction.add(R.id.scroll_bar_container, horizontalScrollBar).commit();
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        */
+        
+        
+
 
 
     }
-    public void setUpButtons()
+
+	public void setUpButtons()
     {
        Button share = (Button)findViewById(R.id.share_button);
        share.setOnClickListener(new ItemClick());
@@ -67,13 +132,60 @@ public class ItemActivity extends Activity {
         TextView text = (TextView) findViewById(R.id.item_genre);
         text.setText("Type: " + item.getGenre());
     }
+    private LinearLayout setUpScrollingItem(View v, MediaItem temp) {
+        LinearLayout scrollingItem = new LinearLayout(v.getContext());
+        scrollingItem.setLayoutParams(new LinearLayout.LayoutParams(188,240));
+        scrollingItem.setOrientation(LinearLayout.VERTICAL);
+        ImageButton button = new ImageButton(scrollingItem.getContext());
+        button.setLayoutParams(new ViewGroup.LayoutParams(150, 200));
+        button.setScaleType(ScaleType.FIT_START);
+        final int myId = temp.getItemID();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ItemActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("id",myId);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        setThumbnailPic(temp, button);
+        TextView t1 = new TextView(v.getContext());
+        t1.setText(temp.getItemName());
+        t1.setTextColor(getResources().getColor(R.color.home_color));
+        t1.setTextSize(12);
+        t1.setGravity(Gravity.BOTTOM);
+        scrollingItem.setMinimumHeight(200);
+        scrollingItem.setMinimumWidth(134);
+        scrollingItem.addView(button);
+        scrollingItem.addView(t1);
+        return scrollingItem;
+    }
+    public void setThumbnailPic(MediaItem item, ImageButton imageButton)
+    {
+        String path = item.getPicture();
+        try {
 
-    private void setTitleName() {
-        setTitle(item.getItemName());
+            // get input stream
+
+            InputStream ims = getAssets().open(path);
+
+            // load image as Drawable
+
+            Drawable d = Drawable.createFromStream(ims, null);
+
+            // set image to ImageView
+            imageButton.setImageDrawable(d);
+        }
+            catch(IOException ex)
+            {
+            	imageButton.setClickable(false);
+            }
     }
 
     private void setItemName(){
-    	TextView text = (TextView) findViewById(R.id.item_name);
+    	TextView text = (TextView) findViewById(R.id.item_name_text);
     	text.setText(item.getItemName());
     }
     private void setItemThumbnail() {
